19cfafa0d6c55c0e5e1284f21d828c4572be9707
gary <gsb.gurpreet@gmail.com>
2020-10-12 12:33:52 -0400

    Add logic to handle the game states: tie, a player won and ongoing

--- app/src/Game.js
+++ app/src/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import "./Game.css"
 
 function Square({ value, onClick }) {
@@ -40,6 +40,7 @@ function Board({ squares, updateSquare, isXNext }) {
 function Game() {
   const [squares, setSquares] = useState(Array(9).fill(""))
   const [isXNext, setIsXNext] = useState(true)
+  const [gmaeInfo, setGameInfo] = useState("Game is going great")
 
   const updateSquare = (idx, isXNext) => {
     // check to stop overriding squares
@@ -51,16 +52,48 @@ function Game() {
     }
   }
 
+  useEffect(() => setGameInfo(gameStatus(squares)), [squares])
+
   return (
     <div className="game">
       <div className="game-board">
         <Board squares={squares} updateSquare={updateSquare} isXNext={isXNext} />
       </div>
       <div className="game-info">
-        <div>{"Game info"}</div>
+        <div>{gmaeInfo}</div>
       </div>
     </div>
   )
+}
+
+function gameStatus(squares) {
+  const winningCombinationsIdx = [
+    // horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonal
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  // check for the combinations
+  for (const combination of winningCombinationsIdx) {
+    const [s0, s1, s2] = [squares[combination[0]], squares[combination[1]], squares[combination[2]]]
+    if (!!s0 && s0 === s1 && s0 === s2) return `Player ${s0} Won!`
+  }
+
+  // if there is no more empty squares, declare tie
+  if (!squares.some((square) => square === "")) {
+    return "tie"
+  }
+
+  // otherwise the game is ongoing
+  return "Ongoing"
 }
 
 export default Game
