1a0c71c60c3891c41b271ab7436efcadd4eb7fa4
gary <gsb.gurpreet@gmail.com>
2020-10-15 11:16:21 -0400

    Improve the look and feel of the app by adding typography and making the grid structure consistant

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,15 +1,20 @@
 import React, { useReducer } from "react"
 import { gameInitalState, gameReducer } from "./GameReducer"
-import { Button, Container, Grid } from "@material-ui/core"
+import { Button, Container, Grid, Typography } from "@material-ui/core"
+
+const style = {
+  height: "100px",
+  fontSize: "50px",
+}
 
 function Board({ squares, onSquareClick }) {
   return (
-    <Grid container direction="row" justify="space-evenly" alignItems="stretch" spacing={3}>
+    <Grid container direction="row" justify="space-evenly" alignItems="stretch" spacing={5}>
       {squares.map((square, i) => (
         <Grid item xs={4} key={i}>
           <Button
             fullWidth
-            style={{ height: "50px" }}
+            style={style}
             size="large"
             variant="outlined"
             color="primary"
@@ -25,18 +30,37 @@ function Board({ squares, onSquareClick }) {
 
 function Game() {
   const [state, dispatch] = useReducer(gameReducer, gameInitalState)
+  const displayText =
+    state.gameInfo +
+    (state.gameInfo === "Ongoing" ? ` next player is ${state.isXNext ? "X" : "O"}` : "")
 
   return (
     <Container>
-      <div>{state.gameInfo + ` Next player: ${state.isXNext ? "X" : "O"}`}</div>
+      <Grid container justify="space-evenly" alignItems="stretch" spacing={5}>
+        <Grid item xs={12}>
+          <Typography align="center" varient="h1" style={style}>
+            {displayText}
+          </Typography>
+        </Grid>
+      </Grid>
       <Board
         squares={state.squares}
         onSquareClick={(idx) => dispatch({ type: "SQUARE_CLICK", idx })}
         isXNext={state.isXNext}
       />
-      <Button variant="contained" color="primary" onClick={() => dispatch({ type: "RESET_GAME" })}>
-        Reset
-      </Button>
+      <Grid container justify="space-evenly" alignItems="stretch" spacing={5}>
+        <Grid item xs={12}>
+          <Button
+            fullWidth
+            style={style}
+            variant="contained"
+            color="primary"
+            onClick={() => dispatch({ type: "RESET_GAME" })}
+          >
+            Reset
+          </Button>
+        </Grid>
+      </Grid>
     </Container>
   )
 }
