a1e071275c53ee44fa3fc25321b7f0f5e48b0aad
gary <gsb.gurpreet@gmail.com>
2020-10-15 11:27:08 -0400

    Updates

---
+++ precache-manifest.72cecc817f7c766bedc979bc47466fdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0876d8ea1d504f86fc6b8613c4ddf2d5",
+    "url": "/tictactoe-with-react-hooks/index.html"
+  },
+  {
+    "revision": "853a9d63379e978f4314",
+    "url": "/tictactoe-with-react-hooks/static/js/2.4044ec30.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/tictactoe-with-react-hooks/static/js/2.4044ec30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18c668bd1cd944f96da",
+    "url": "/tictactoe-with-react-hooks/static/js/main.ecd97cc7.chunk.js"
+  },
+  {
+    "revision": "8243b4866fad4232c53b",
+    "url": "/tictactoe-with-react-hooks/static/js/runtime-main.e4b97e5f.js"
+  }
+]);
